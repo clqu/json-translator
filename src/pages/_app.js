84783af7897ddc67f2bb9d5340b60e20dd36fed4
uh! <76551996+clqu@users.noreply.github.com>
2022-04-20 19:12:27 +0300

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,8 +7,7 @@ function MyApp({ Component, pageProps }) {
     </div>
     <div style={{ zIndex: 100 }} className="fixed bottom-5 left-5">
       <p>Developed with ❤️ by 
-        <a href="https://clqu.live" className="hover:text-purple-500 font-medium transition-all duration-200" target="_blank"> clqu </a> & 
-        <a href="https://swoth.xyz" className="hover:text-purple-500 font-medium transition-all duration-200" target="_blank"> Swôth </a></p>
+        <a href="https://clqu.live" className="hover:text-purple-500 font-medium transition-all duration-200" target="_blank"> clqu </a>
     </div>
     <div className="layout-colored-background fixed top-0 opacity-10" />
   </main>
